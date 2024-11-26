010ddc5610049d850e86bddcc5aa733e3425de7b
Luisfer Romero Calero <luisfer.romero.calero@gmail.com>
2024-11-26 02:31:14 +0100

    feat: Add PodcastSidebar component - Create reusable sidebar for podcast details - Add custom styling with CSS modules - Make title and author clickable - Implement responsive design

---
+++ podcast-app/src/app/components/PodcastSidebar/PodcastSidebar.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import styles from './PodcastSidebar.module.css';
+
+interface PodcastSidebarProps {
+  id: string;
+  title: string;
+  author: string;
+  image: string;
+  description: string;
+}
+
+export function PodcastSidebar({ 
+  id, 
+  title, 
+  author, 
+  image, 
+  description 
+}: PodcastSidebarProps) {
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.imageContainer}>
+        <Link href={`/podcast/${id}`}>
+          <img 
+            src={image} 
+            alt={`${title} cover`} 
+            className={styles.image}
+          />
+        </Link>
+      </div>
+      
+      <div className={styles.infoContainer}>
+        <Link 
+          href={`/podcast/${id}`} 
+          className={styles.title}
+        >
+          {title}
+        </Link>
+        <br />
+        <Link 
+          href={`/podcast/${id}`} 
+          className={styles.author}
+        >
+          by {author}
+        </Link>
+      </div>
+
+      <div className={styles.description}>
+        <h3>Description:</h3>
+        <p>{description}</p>
+      </div>
+    </aside>
+  );
+}
---
+++ podcast-app/src/app/components/PodcastSidebar/index.ts
@@ -0,0 +1,1 @@
+export { PodcastSidebar } from './PodcastSidebar';
